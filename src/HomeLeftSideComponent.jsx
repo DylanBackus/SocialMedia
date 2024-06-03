837fb877a3ce615734a665e03d94f1c48e1cd599
Selcuk <1202320@student.roc-nijmegen.nl>
2024-06-03 11:00:52 +0200

    sevcond commit

--- src/HomeLeftSideComponent.jsx
+++ src/HomeLeftSideComponent.jsx
@@ -5,9 +5,20 @@ const HomeLeftSideComponent = () => {
     return(
         <>
         <div className="home-container">
-            <div className="per33"></div>
+            <div className="main-left">
+                <div className="profile-area">
+                  <img src="images/placeholder.png" className="pfp"/> 
+                  <div className="ro"> 
+                    <div className="profile-icon">
+                        <a href="profile" className="max-size">
+                            <img src="images/user.png"  className="max-size"/>
+                        </a>
+                    </div>
+                  </div>
+                </div>
+            </div>
             <div className="mid-home"></div>
-            <div className="per33"></div>
+            <div className="search-area"></div>
         </div>
         </>
     )
