7ea4585661aef25d23174f87362777a81e8e14c0
Selcuk <1202320@student.roc-nijmegen.nl>
2024-06-03 16:28:34 +0200

    commit

--- src/HomeLeftSideComponent.jsx
+++ src/HomeLeftSideComponent.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import db from './firebase/FirebaseConfig'
-import { doc, getDocFromCache } from "firebase/firestore";
+import { doc, getDocFromCache, getDoc } from "firebase/firestore";
 
 
+const docRef = doc(db, "users", "Q3LricYjI89T1CmqlBoG",);
+const docSnap = await getDoc(docRef);
+
+if (docSnap.exists()) {
+  console.log("Document data:", docSnap.data());
+  console.log(docSnap.data(1))
+} else {
+  // docSnap.data() will be undefined in this case
+  console.log("No such document!");
+}
 const HomeLeftSideComponent = () => {
 
     return (
