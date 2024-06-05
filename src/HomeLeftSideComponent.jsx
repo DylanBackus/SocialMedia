912468e97446211307197a5e0d44cc16630a4807
Dylan Backus <duradylan@gmail.com>
2024-06-05 14:42:58 +0200

    end of day commit

--- src/HomeLeftSideComponent.jsx
+++ src/HomeLeftSideComponent.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect } from "react";
-import db from './firebase/FirebaseConfig'
+import db from "./firebase/FirebaseConfig";
 import { doc, getDocFromCache, getDoc } from "firebase/firestore";
 import { useState } from "react";
 
 const HomeLeftSideComponent = () => {
-
-
   const [userData, setUserData] = useState(null);
-  const userID = ""
+  const userID = "";
   const docRef = doc(db, "users", "Q3LricYjI89T1CmqlBoG");
   //const usersCollectionRef = collection(db, "users");
 
@@ -19,38 +17,34 @@ const HomeLeftSideComponent = () => {
       // docSnap.data() will be undefined in this case
       console.log("empty");
     }
-  }
+  };
   useEffect(() => {
     getData();
-  },[]);
+  }, []);
 
-    return (
-        <>
-            <div className="main-left">
-                <div className="profile-area">
-                  <img src="images/placeholder.png" className="pfp"/> 
-                  <p className="home-username">
-                  @{userData && userData.username}
-                  </p>
-                  <div className="row"> 
-                    <div className="profile-icon">
-                        <a href="profile" className="max-size">
-                            <img src="images/user.png"  className="max-size2"/>
-                        </a>
-                        <a href="profile" className="max-size">
-                            <img src="images/bookmark.png"  className="max-size2"/>
-                        </a>
-                        <a href="profile" className="max-size">
-                            <img src="images/settings.png"  className="max-size2"/>
-                        </a>
-                    </div>
-                  </div>
-                  <div className="friends-area-home">
-
-                  </div>
-                </div>
+  return (
+    <>
+      <div className="main-left">
+        <div className="profile-area">
+          <img src="images/placeholder.png" className="pfp" />
+          <p className="home-username">@{userData && userData.username}</p>
+          <div className="row">
+            <div className="profile-icon">
+              <a href="profile" className="max-size">
+                <img src="images/user.png" className="max-size2" />
+              </a>
+              <a href="profile" className="max-size">
+                <img src="images/bookmark.png" className="max-size2" />
+              </a>
+              <a href="profile" className="max-size">
+                <img src="images/settings.png" className="max-size2" />
+              </a>
             </div>
-        </>
-    )
-}
-export default HomeLeftSideComponent
+          </div>
+          <div className="friends-area-home"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default HomeLeftSideComponent;
--- src/HomeRightSideComponent.jsx
+++ src/HomeRightSideComponent.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 
 const HomeRightSideComponent = () => {
-
-    return (
-        <>
-            <div className="right-home">
-                <input className="right-home-search" placeholder="Type Here..."></input>
-                <div className="daily-trends-main-container">
-                    <h1 className="daily-trends">Daily Trends</h1>
-                    <div className="daily-trends-container">
-                        <h1 className="test">TEST TEST</h1>
-                    </div>
-                </div>
-            </div>
-
-        </>
-    )
-}
-export default HomeRightSideComponent
+  return (
+    <>
+      <div className="right-home">
+        <input className="right-home-search" placeholder="Type Here..."></input>
+        <div className="daily-trends-main-container">
+          <h1 className="daily-trends">Daily Trends</h1>
+          <div className="daily-trends-container">
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+            <h1 className="test">TEST TEST</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default HomeRightSideComponent;
