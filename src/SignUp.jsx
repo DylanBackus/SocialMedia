381512f094c430275b3adb910f6622b2aeef6e80
Selcuk <1202320@student.roc-nijmegen.nl>
2024-06-12 13:01:17 +0200

    fixed multiple ppl at once going into the user database

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,42 +1,58 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebase/FirebaseConfig";
 import { useState } from "react";
-const SignUp = () => {
-    const SignUpFunc = async() => {
-        const email = document.getElementById("mail").value;
-        const password = document.getElementById("password").value;
-        createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // Signed up 
-          const user = userCredential.user;
-          // ...
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // ..
-        });
 
+const SignUp = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    const SignUpFunc = () => {
+        createUserWithEmailAndPassword(auth, email, username, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
     }
-    
-    return(
-        <>
-        <div className="signUpbg">
-            <div className="signUpContainer">
-                <h2>Sign Up</h2>
-                <form action="#" className="signInForm">
-                    <label htmlFor="html">Email address</label>
-                    <input type="email" className="inputField" id="mail"/>
-                    <label htmlFor="html">Username</label>
-                    <input type="text" className="inputField" id="username"/>
-                    <label htmlFor="html">password</label>
-                    <input type="pass" className="inputField" id="password"/>
-                    <button type="button" onClick={SignUpFunc()} >sign up</button>
-                </form>
-                
-            </div>
+
+  return (
+    <>
+      <div className="signUpbg">
+        <div className="signUpContainer">
+          <h2>Sign Up</h2>
+          <form onSubmit={() => SignUpFunc()} action="login" className="signInForm">
+            <label htmlFor="html">Email address</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="inputField"
+            />
+            <label htmlFor="html">Username</label>
+            <input
+              type="text"
+              className="inputField"
+            />
+            <label htmlFor="html">password</label>
+            <input
+              type="pass"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="inputField"
+            />
+            <button type="submit">
+              sign up
+            </button>
+          </form>
         </div>
-        </>
-    )
-}
-export default SignUp
+      </div>
+    </>
+  );
+};
+export default SignUp;
