18e302fa662fd90ceb194a0df93ef606a9c53eb9
Selcuk <1202320@student.roc-nijmegen.nl>
2024-06-13 10:19:04 +0200

    signup

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,61 +1,71 @@
+import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "./firebase/FirebaseConfig";
-import { useState } from "react";
-import { ref, set } from "firebase/database";
+import { doc, setDoc } from "firebase/firestore"; 
 
 const SignUp = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const writeUserData = async (mail, uid) => {
+            await setDoc(doc(db, "users", uid), {
+                email: mail,
+                name: ""
+            });
+            console.log("Data saved successfully!");
+    };
 
-    const SignUpFunc = () => {
+    const SignUpFunc = (event) => {
+        event.preventDefault(); 
         createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed up
-        const user = userCredential.user;
-        const mail = userCredential.email;
-        const uid = userCredential.uid;
+        .then((userCredential) => {
+            // Signed up
+            const user = userCredential.user;
+            const mail = user.email;
+            const uid = user.uid;
+            writeUserData(mail, uid);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+    };
 
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
-    }
-
-  return (
-    <>
-      <div className="signUpbg">
-        <div className="signUpContainer">
-          <h2>Sign Up</h2>
-          <form onSubmit={() => SignUpFunc()} className="signInForm">
-            <label htmlFor="html">Email address</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="inputField"
-            />
-            <label htmlFor="html">Username</label>
-            <input  
-              type="text"
-              className="inputField"
-            />
-            <label htmlFor="html">password</label>
-            <input
-              type="pass"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="inputField"
-            />
-            <button type="submit" className="submitButton">
-              Sign Up
-            </button>
-          </form>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="signUpbg">
+                <div className="signUpContainer">
+                    <h2>Sign Up</h2>
+                    <form onSubmit={SignUpFunc} className="signInForm">
+                        <label htmlFor="email">Email address</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="inputField"
+                        />
+                        <label htmlFor="username">Username</label>
+                        <input  
+                            type="text"
+                            id="username"
+                            className="inputField"
+                        />
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="inputField"
+                        />
+                        <button type="submit" className="submitButton">
+                            Sign Up
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
 };
+
 export default SignUp;
