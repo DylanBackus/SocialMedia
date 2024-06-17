b1b9063efd7a75f03b8778fec6e95562db376f29
SergioGomezAlvarez <sgmichielr@gmail.com>
2024-06-17 12:10:23 +0200

    added profile page

---
+++ src/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import HomeLeftSideComponent from "./HomeLeftSideComponent";
+import HomeMidComponent from "./HomeMidComponent";
+import HomeRightSideComponent from "./HomeRightSideComponent";
+import { db, auth } from '../firebase/FirebaseConfig';
+import { onAuthStateChanged } from "firebase/auth";
+const Home = () => {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/auth.user
+            const uid = user.uid;
+            // ...
+        } else {
+            window.location = "signup"
+        }
+    });
+    return (
+        <>
+            <div className="home-container">
+                <HomeLeftSideComponent />
+                <HomeMidComponent />
+                <HomeRightSideComponent />
+            </div>
+        </>
+    )
+}
+export default Profile
